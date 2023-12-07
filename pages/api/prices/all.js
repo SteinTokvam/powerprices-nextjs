91face495ca0f353443fe9db379c2e2dd1b9035e
Stein Petter Tokvam <stto1210@gmail.com>
2023-12-07 14:22:44 +0100

    package bump and some fixes

--- pages/api/prices/all.js
+++ pages/api/prices/all.js
@@ -2,7 +2,7 @@ import { makeList } from '../../../util/powerUtil';
 
 export default async function handler(req, res) {
     const { zone } = req.query;
-        const result = await (makeList(zone, false, new Date(until)));
+        const result = await (makeList(zone, false, new Date(new Date().setDate(new Date().getDate() + 2))));
         if(result.status) {
             res.status(result.status).json({ message: result.message });
         }
--- pages/api/prices/custom.js
+++ pages/api/prices/custom.js
@@ -2,7 +2,7 @@ import { makeList } from '../../../util/powerUtil';
 
 export default async function handler(req, res) {
     const { zone, hasPassed, until } = req.body;
-    console.log(req.body)
+    
     const result = await makeList(zone, hasPassed === 'true', until);
     if(result.status) {
         res.status(result.status).json({ message: result.message });
